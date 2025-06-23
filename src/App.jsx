2d7e4daba87eb934cdad4083787bb46300a0c24b
Mallory Joy <malloryjoy13@gmail.com>
2025-06-23 18:51:15 -0400

    Adding result page

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,9 @@ function App() {
             
             
            
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       
       
-      <h1>Vite + React Mallory says hello! </h1>
+      
      </div>
     </>
   )
