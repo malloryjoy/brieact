4fa461580e93623b1864323d816fe6fa91db0442
Mallory Joy <malloryjoy13@gmail.com>
2025-06-26 17:19:38 -0400

    Adding submitted conditional

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,32 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import MyForm from './components/myForm';
+import Board from './Board.jsx'
 
+
+
+
+/*if submitted == true {
+  show Board
+}
+
+else {
+  show Form
+} 
+  
+
+LOOK UP USE STATE PROCESS I KNOW YOU ALREADY DID THAT BUT IT'S OVER THERE */
 
 function App() {
   
-
+  const [submitted,setSubmitted] = useState(false);
   return (<>
       <div>
       
     
 
                <MyForm />
+               <Board />
             
             
            
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Board from './Board.jsx'
+
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Board />
+    
   </StrictMode>,
 )
 
+
+{/*So App is the form that asks users to provide the cheese and the filters, and Board is the cheese
+  board that is generated from their answers. If I'm loading everything on one page, how and where 
+  should I say to hide the Board section when the App is open and vice versa? I would think adding 
+  an Event Listener to the App's submit button would do the trick, but I feel like there needs to be an
+  initializer that hides the Board when you open the page*/}
 
 
 
