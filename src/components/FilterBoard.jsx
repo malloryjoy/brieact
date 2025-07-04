a805e0186bd74bbd58a720927e4aec531bfe3a30
Mallory Joy <malloryjoy13@gmail.com>
2025-07-04 13:01:46 -0400

    Updated CSS to repair checkbox sleection on mobile

--- src/components/FilterBoard.jsx
+++ src/components/FilterBoard.jsx
@@ -6,10 +6,10 @@ class FilterBoard extends React.Component {
       const { handleCheckboxChange } = this.props;
       return (<><section id="filter">
       <h3 style={{textAlign: "left", fontSize: "1.25em", color:"white"}}>And Make It...</h3><br />
-      <div id="inputSection" style={{float: "left"}}>
-      <input type="checkbox" id="filter1" name="filter1" onChange={handleCheckboxChange} value="Vegetarian" /><label htmlFor="filter1"> Vegetarian</label> {/*vegetarian*/}
-      <input type="checkbox" id="filter2" name="filter2" onChange={handleCheckboxChange} value="Nut-Free" /><label htmlFor="filter2"> Nut-Free</label> {/*nutFree*/}
-      <input type="checkbox" id="filter3" name="filter3" onChange={handleCheckboxChange} value="Zero-Proof" /><label htmlFor="filter3"> Zero-Proof</label>{/*zeroProof*/}
+      <div id="inputSection" >
+      <label htmlFor="filter1"><input type="checkbox" id="filter1" name="filter1" onChange={handleCheckboxChange} value="Vegetarian" /> Vegetarian</label> {/*vegetarian*/}
+      <label htmlFor="filter2"><input type="checkbox" id="filter2" name="filter2" onChange={handleCheckboxChange} value="Nut-Free" /> Nut-Free</label> {/*nutFree*/}
+      <label htmlFor="filter3"><input type="checkbox" id="filter3" name="filter3" onChange={handleCheckboxChange} value="Zero-Proof" /> Zero-Proof</label>{/*zeroProof*/}
       </div></section>
       </>)
     }
